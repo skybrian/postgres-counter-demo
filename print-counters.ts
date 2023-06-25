d102b874bf4a6c839811154f0dd08df80154f97a
Brian Slesinsky <brian-git@slesinsky.org>
2023-06-24 23:12:26 -0700

    initial setup with deno fresh skeleton

---
+++ print-counters.ts
@@ -0,0 +1,8 @@
+import { Pool } from 'npm:@neondatabase/serverless';
+
+const pool = new Pool({ connectionString: Deno.env.get("DATABASE_URL") });
+
+const rs = await pool.query('SELECT key, count from counters');
+console.log(rs.rows);
+
+await pool.end();
---
+++ routes/index.tsx
@@ -0,0 +1,11 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Postgres counter demo</title>
+            </Head>
+        </>
+    );
+}
