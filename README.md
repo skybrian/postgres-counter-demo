4bd8310564427165e2e4c351c3358b178e6df222
Brian Slesinsky <brian-git@slesinsky.org>
2023-06-25 23:59:36 -0700

    explain what it is

---
+++ README.md
@@ -0,0 +1,7 @@
+A simple demonstration of how to build an old-fashioned database-backed website using the free tiers of some fairly new Internet services. (New as of 2023.)
+
+Here are the technologies I used:
+
+* Deno Deploy: web hosting
+* Deno Fresh: web framework (server-side only for this demo)
+* Neon: Postgres database hosting
--- routes/index.tsx
+++ routes/index.tsx
@@ -22,6 +22,19 @@ export default function renderPage({ data }: PageProps<Counter[]>) {
                 <h1>Postgres counter demo</h1>
 
                 {data ? renderForm(data) : "unable to load counters"}
+
+                <p>This page is a simple demonstration of how to build an old-fashioned database-backed website
+                  using the free tiers of some fairly new Internet services. (New as of 2023.)</p>
+
+                <p>Here are the technologies I used:</p>
+
+                <ul>
+                    <li><a href="https://deno.com/deploy">Deno Deploy:</a> web hosting</li>
+                    <li><a href="https://fresh.deno.dev/">Deno Fresh:</a> web framework (server-side only for this demo)</li>
+                    <li><a href="https://neon.tech/">Neon:</a> Postgres database hosting</li>
+                </ul>
+
+                <p>Source is available on <a href="https://github.com/skybrian/postgres-counter-demo">Github.</a></p>
             </div>
         </>
     );
