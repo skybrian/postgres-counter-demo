24668222458586d2ad35429a33a118b8f21069e7
Brian Slesinsky <brian-git@slesinsky.org>
2023-06-25 23:24:57 -0700

    add css and make pretty

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,5 @@
 import { HandlerContext, Handlers, PageProps } from "$fresh/server.ts";
-import { Head } from "$fresh/runtime.ts";
+import { Head, asset } from "$fresh/runtime.ts";
 
 import { query } from "../lib/database.ts";
 import { Counter } from "../lib/schema.ts";
@@ -15,7 +15,8 @@ export default function renderPage({ data }: PageProps<Counter[]>) {
     return (
         <>
             <Head>
-                <title>Postgres counter demo</title>
+                <title>Postgres counter demo!</title>
+                <link rel="stylesheet" href={asset("/global.css")}/>
             </Head>
             <div>
                 <h1>Postgres counter demo</h1>
@@ -27,7 +28,7 @@ export default function renderPage({ data }: PageProps<Counter[]>) {
 }
 
 const renderForm = (rows: Counter[]) =>
-    <form method="POST" action="/increment">{rows.map(renderButton)}</form>
+    <form class="buttons" method="POST" action="/increment">{rows.map(renderButton)}</form>
 
 const renderButton = (c: Counter) =>
     <button name="button" value={c.key}>{c.count} {c.symbol}</button>
