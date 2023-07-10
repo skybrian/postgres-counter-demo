b042b64de7302bcc8d83e519331b8dccb98f7dc1
Brian Slesinsky <brian-git@slesinsky.org>
2023-07-09 21:00:52 -0700

    add log messages for lifecycle

--- main.ts
+++ main.ts
@@ -6,7 +6,19 @@
 
 import "$std/dotenv/load.ts";
 
+import { startLog } from "./lib/log.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
+const log = startLog("lifecycle");
+
+globalThis.addEventListener("unload", () => {
+  log.sendTime("unload event");
+});
+
+globalThis.addEventListener("unhandledrejection", (e) => {
+  log.send(`unhandled rejection at: ${e.promise}, reason: ${e.reason}`);
+});
+
 await start(manifest);
+log.sendTime("fresh framework exited");
